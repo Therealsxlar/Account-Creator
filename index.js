304f5172a0d5787bbfc2c76d884e65dffec49bb5
Therealsxlar <104775507+Therealsxlar@users.noreply.github.com>
2024-03-31 14:05:59 -0400

    Uhh I forgot this for yall

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 const fs = require('fs');
 const Discord = require('discord.js');
+const { config } = require('process');
 const client = new Discord.Client();
 client.commands = new Discord.Collection();
+const Config = JSON.parse(fs.readFileSync('config.json'));
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
@@ -9,6 +11,10 @@ for (const file of commandFiles) {
     const command = require(`./commands/${file}`);
     client.commands.set(command.name, command);
 }
+
+client.on('ready', () => {
+    console.log('\x1b[31m%s\x1b[0m', `${client.user.tag} is online!`);
+});
 
 client.on('message', message => {
     if (!message.content.startsWith('!') || message.author.bot) return;
@@ -26,4 +32,4 @@ client.on('message', message => {
     }
 });
 
-client.login('[YOUR TOKEN HERE]');
+client.login(Config.Token);
